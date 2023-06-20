720c2791d95a4ddfa02d02d1e2e0f680974672b3
akramab <13519142@std.stei.itb.ac.id>
2023-06-20 18:56:39 +0700

    add mock api response for sprint 1

--- controllers/infrastructure.ts
+++ controllers/infrastructure.ts
@@ -28,11 +28,77 @@ export const getAllInfrastructure = async (req: Request, res: Response) => {
         const {total_items, total_page, current_page,items } = getPagingData(infrastructure, page, size)
         
         res.send({
-            total_items,
-            total_page,
-            current_page,
-            items,
-        })
+            "total_items": 4,
+            "total_page": 1,
+            "page": 1,
+            "items": [
+              {
+                "type": "air_bersih",
+                "name": "Titik Air Bersih Rumah Pak Yoga",
+                "description": {
+                  "ownership": "sendiri",
+                  "source": "sumur_air_tanah",
+                  "service_scope": {
+                    "capacity": 200.5,
+                    "kk_count": 1,
+                    "people_count": 10
+                  },
+                  "monthly_service_bill": 190.5,
+                  "contact_person": "+628121443294"
+                }
+              },
+              {
+                "type": "air_bersih",
+                "name": "Titik Air Bersih RW 06",
+                "description": {
+                  "ownership": "bersama",
+                  "stakeholder": "Pengurus RW",
+                  "source": "pdam",
+                  "service_scope": {
+                    "capacity": 2000,
+                    "kk_count": 20,
+                    "people_count": 100
+                  },
+                  "monthly_service_bill": 2000000,
+                  "contact_person": "+62812211321"
+                }
+              },
+              {
+                "type": "air_kotor",
+                "name": "Jamban RW 06",
+                "description": {
+                  "ownership": "bersama",
+                  "stakeholder": "Pengurus RW",
+                  "toilet_type": "plengsengan",
+                  "water_processing_type": "septic_tank",
+                  "service_scope": {
+                    "capacity": 100,
+                    "kk_count": 20,
+                    "people_count": 100
+                  },
+                  "monthly_service_bill": 50000,
+                  "contact_person": "+628190202312"
+                }
+              },
+              {
+                "type": "titik_persampahan",
+                "name": "Titik Pembuangan Sampah RW 06",
+                "description": {
+                  "ownership": "bersama",
+                  "stakeholder": "Pengurus RW",
+                  "waste_type": "tps",
+                  "waste_processing_type": "individual_langsung",
+                  "service_scope": {
+                    "capacity": 1000,
+                    "kk_count": 20,
+                    "people_count": 100
+                  },
+                  "monthly_service_bill": 100000,
+                  "contact_person": "+62812102293"
+                }
+              }
+            ]
+          })
 
     } catch (error) {
         console.log(error)
@@ -50,4 +116,49 @@ export const getInfrastructureByID = async (req: Request, res: Response) => {
         res.send(infrastructures[0])
     } catch (error) {
     }
+}
+
+export const createInfrastructure = async (req: Request, res: Response) => {
+    res.send({
+        "message": "infrastructure has been succesfully created",
+        "id": "497f6eca-6276-4993-bfeb-53cbbbba6f08"
+    })
+}
+
+export const getInfrastructureTypeList = async (req: Request, res: Response) => {
+    res.send([
+        {
+          "id": 1,
+          "type": "titik",
+          "icon_url": "https://www.myimage/titik_icon.svg",
+          "sub_type": [
+            "air_bersih",
+            "air_limbah"
+          ]
+        },
+        {
+          "id": 2,
+          "type": "garis",
+          "icon_url": "https://www.myimage/garis_icon.svg",
+          "sub_type": [
+            "jalan",
+            "drainase"
+          ]
+        }
+      ])
+}
+
+export const getInfrastructureSubTypeList = async (req: Request, res: Response) => {
+    res.send([
+        {
+          "id": 1,
+          "sub_type": "air_bersih",
+          "icon_url": "https://www.myimage.com/air_bersih_icon.svg"
+        },
+        {
+          "id": 2,
+          "sub_type": "air_kotor",
+          "icon_url": "https://www.myimage.com/air_kotor_icon.svg"
+        }
+      ])
 }
--- routes/infrastructure.ts
+++ routes/infrastructure.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from "express";
-import { getAllInfrastructure, getInfrastructureByID } from "../controllers/infrastructure";
+import { getAllInfrastructure, getInfrastructureByID, createInfrastructure, getInfrastructureTypeList, getInfrastructureSubTypeList } from "../controllers/infrastructure";
 
 const InfrastructureRouter = Router();
 
-InfrastructureRouter.get('/data', getAllInfrastructure)
+// MOCK IMPLEMENTATION
+InfrastructureRouter.get('/', getAllInfrastructure)
 InfrastructureRouter.get('/data/:id', getInfrastructureByID)
+InfrastructureRouter.post('/', createInfrastructure)
+InfrastructureRouter.get('/type', getInfrastructureTypeList)
+InfrastructureRouter.get('/type/:id/sub', getInfrastructureSubTypeList)
 
 export default InfrastructureRouter;
